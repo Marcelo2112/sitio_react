eebcc4231912235670be7918cdbe7e1b32a2a804
Marcelo2112 <allieri01@gmail.com>
2020-07-20 16:49:54 -0400

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export function login (usuario){
-    fetch('http://localhost:5000/login',{
+    return fetch('http://localhost:5000/login',{
         method: 'POST',
         body: JSON.stringify({
             correo: usuario.email,
@@ -20,5 +20,5 @@ export function getDeliveries(){
             'Authorization': 'Bearer' +token
         }
 
-    })
+    });
 }
